71b642d13c69854867d7459914dc9d0b2f820ab0
Lori A. Burns <lori.burns@gmail.com>
2021-03-22 14:34:03 -0400

    forward 53

--- conda/recipe/bld.bat
+++ conda/recipe/bld.bat
@@ -39,17 +39,17 @@ echo %LIBRARY_PATH%
 
 :: set pkg-config path so that host deps can be found
 :: (set as env var so it's used by both meson and during build with g-ir-scanner)
-set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig"
+::::set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig"
 
 :: get mixed path (forward slash) form of prefix so host prefix replacement works
 set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
 
+::  --pkg-config-path="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig" ^
 
 set ^"MESON_OPTIONS=^
   --prefix="%LIBRARY_PREFIX_M%" ^
   --buildtype=release ^
   --backend=ninja ^
-  --pkg-config-path="%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig" ^
   --errorlogs ^
   -D openmp=false ^
   -D python=true ^
@@ -68,18 +68,18 @@ echo !MESON_OPTIONS!
 
 :: configure build using meson
 :: %BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\
-meson setup builddir !MESON_OPTIONS!
+meson setup _build !MESON_OPTIONS!
 if errorlevel 1 exit 1
 
 :: print results of build configuration
 :: %BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\
-meson configure builddir
+meson configure _build
 if errorlevel 1 exit 1
 
-ninja -v -C builddir -j %CPU_COUNT%
+ninja -v -C _build -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
-ninja -C builddir install -j %CPU_COUNT%
+ninja -C _build install -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
 :: :: configure build using meson
